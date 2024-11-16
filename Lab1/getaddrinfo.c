bddd121bfa96b468fc62da46e1f57f168c63cc85
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-11-16 20:53:44 +0000

    Added udp

--- Lab1/getaddrinfo.c
+++ Lab1/getaddrinfo.c
@@ -1,5 +1,5 @@
-//getaddrinfo.c
-// Task 2
+// getaddrinfo.c
+//  Task 2
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,6 +11,30 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-int main(void) {
+int main(void)
+{
   struct addrinfo hints, *res, *p;
+  int errcode;
+  char buffer[INET_ADDRSTRLEN];
+  struct in_addr *addr;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET; // IPv4
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_CANONNAME;
+  
+  if ((errcode = getaddrinfo("Versailles", NULL, &hints, &res)) != 0)
+    fprintf(stderr, "error: getaddrinfo: %s\n", gai_strerror(errcode));
+  else
+  {
+    printf("canonical hostname: %s\n", res->ai_canonname);
+    for (p = res; p != NULL; p = p->ai_next)
+    {
+      addr = &((struct sockaddr_in *)p->ai_addr)->sin_addr;
+      printf("internet address: %s (%08lX)\n",
+             inet_ntop(p->ai_family, addr, buffer, sizeof buffer),
+             (long unsigned int)ntohl(addr->s_addr));
+    }
+    freeaddrinfo(res);
+  }
+  exit(0);
 }
---
+++ Lab1/udpserver.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdio.h>
+
+#define PORT "58001"
+
+int fd, errcode;
+ssize_t n;
+socklen_t addrlen;
+struct addrinfo hints, *res;
+struct sockaddr_in addr;
+char buffer[128];
+char host[NI_MAXHOST], service[NI_MAXSERV]; // consts in <netdb.h>
+
+int main(void)
+{
+  fd = socket(AF_INET, SOCK_DGRAM, 0); // UDP socket
+  if (fd == -1)                        /*error*/
+    exit(1);
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;      // IPv4
+  hints.ai_socktype = SOCK_DGRAM; // UDP socket
+  hints.ai_flags = AI_PASSIVE;
+  errcode = getaddrinfo(NULL, PORT, &hints, &res);
+  if (errcode != 0) /*error*/
+    exit(1);
+
+  n = bind(fd, res->ai_addr, res->ai_addrlen);
+  if (n == -1) /*error*/
+    exit(1);
+
+  while (1)
+  {
+    addrlen = sizeof(addr);
+    n = recvfrom(fd, buffer, 128, 0,
+                 (struct sockaddr *)&addr, &addrlen);
+    if (n == -1) /*error*/
+      exit(1);
+    write(1, "received: ", 10);
+    write(1, buffer, n);
+
+    n = sendto(fd, buffer, n, 0,
+               (struct sockaddr *)&addr, addrlen);
+    if (n == -1) /*error*/
+      exit(1);
+
+    if ((errcode = getnameinfo((struct sockaddr *)&addr, addrlen, host, sizeof host, service, sizeof service, 0)) != 0)
+      fprintf(stderr, "error: getnameinfo: %s\n", gai_strerror(errcode));
+    else
+      printf("sent by [%s:%s]\n", host, service);
+  }
+
+  freeaddrinfo(res);
+  close(fd);
+
+  return 0;
+}
